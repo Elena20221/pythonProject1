ee150418d7676699450f5a3fd365a76e08b14ff3
Elena <gladkihelena0110@gmail.com>
2022-10-02 14:42:53 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+def multiplication_int(a: int,b:int)->int:
+    return a*b
+
+
+def multiplication_string(line:str,n:int)->str:
+    return line*n
+
+
+result = multiplication_int(2,3)
+print(result)
+
+result = multiplication_string("V",10)
+print(result)
+
+
+
+
+
+
+
